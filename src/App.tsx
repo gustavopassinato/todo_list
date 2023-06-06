ecc5fe97c378687df9dd4f1cae554979f4f7721d
Gustavo Passinato Neves <passinato11@gmail.com>
2023-06-06 20:19:22 -0300

    Finish of the input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { Header } from './components/Header'
+import { TaskInput } from './components/TaskInput'
+
+import styles from './App.module.css'
 
 export function App() {
   return (
     <>
       <Header />
+      <main className={styles.main}>
+        <TaskInput />
+      </main>
     </>
   )
 }
